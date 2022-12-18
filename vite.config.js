1b36d87c1e6612da14cc0a954f1858585c3a76ff
Shuntaro Nishizawa <me@shun.technology>
2022-12-19 08:51:42 +0900

    build: Upgrade dependencies

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { minifyHtml } from "vite-plugin-html";
-
-export default {
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
-  plugins: [minifyHtml()],
-};
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
+});
