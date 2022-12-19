8b77c1523497405e5b0dcff70d887b10ec6cb003
Shuntaro Nishizawa <me@shun.technology>
2022-12-19 09:17:48 +0900

    feat: Migrate react rendering api

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,9 @@
-import type { VFC } from "react";
-
-const App: VFC = () => {
+function App(): JSX.Element {
   return (
     <div className="absolute w-full h-full grid place-items-center">
       <h1 className="text-4xl text-teal-400">Hello, world!</h1>
     </div>
   );
-};
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import "tailwindcss/tailwind.css";
 
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 
 import App from "~/components/App";
 
-render(<App />, document.body);
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
