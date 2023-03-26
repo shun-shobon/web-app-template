5cb576a6cd5d2f8d310ef60b6dfef0781a12df4e
Shuntaro Nishizawa <me@shun.technology>
2023-03-26 16:20:56 +0900

    chore: Use @tsconfig/bases

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { createHtmlPlugin } from "vite-plugin-html";
-import { resolve } from "node:path";
-
-import tsconfig from "./tsconfig.json" assert { type: "json" };
-
-const alias = Object.fromEntries(
-  Object.entries(tsconfig.compilerOptions.paths).map(([key, value]) => [
-    key.replace("/*", ""),
-    resolve(
-      __dirname,
-      tsconfig.compilerOptions.baseUrl,
-      value[0]!.replace("/*", ""),
-    ),
-  ]),
-);
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
@@ -22,8 +9,6 @@ export default defineConfig({
     createHtmlPlugin({
       minify: true,
     }),
+    tsconfigPaths(),
   ],
-  resolve: {
-    alias,
-  },
 });
