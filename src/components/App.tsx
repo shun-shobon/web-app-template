a9683541e6daebf5237a1f1cfd9ed1933f1cecd1
Shuntaro Nishizawa <me@shun.technology>
2023-03-26 16:22:10 +0900

    docs: Supress expected error

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import { VStack, Heading } from "@chakra-ui/react";
 
 function App(): JSX.Element {
   return (
+    // @ts-expect-error: https://github.com/chakra-ui/chakra-ui/issues/7459
     <VStack w="100wh" h="100vh" justify="center">
       <Heading as="h1" size="4xl" color="teal.300">
         Hello, world!
