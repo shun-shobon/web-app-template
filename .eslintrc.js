16efe450ea5b948fbf34a898849334072c863cdd
Shuntaro Nishizawa <me@shun.technology>
2023-03-31 14:13:08 +0900

    chore: Refactor linter and formatter settings

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["@shun-shobon/base", "@shun-shobon/react"],
-  env: {
-    browser: true,
-    node: true,
-  },
-};
--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  trailingComma: "all",
-  plugins: [require("@trivago/prettier-plugin-sort-imports")],
-  importOrder: ["<THIRD_PARTY_MODULES>", "^~/(.*)$", "^[./]"],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-};
