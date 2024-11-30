91684eba22c3773d777d73027c0bdf0e3ba6b338
NISHIZAWA Shuntaro <me@s2n.tech>
2024-11-30 10:59:15 +0900

    chore: Change linter / formatter package

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { Heading, VStack } from "@chakra-ui/react";
+import type { FC } from "react";
 
-function App(): JSX.Element {
+const App: FC = () => {
 	return (
 		<VStack w="100wh" h="100vh" justify="center">
 			<Heading as="h1" size="4xl" color="teal.300">
@@ -8,6 +9,6 @@ function App(): JSX.Element {
 			</Heading>
 		</VStack>
 	);
-}
+};
 
 export default App;
