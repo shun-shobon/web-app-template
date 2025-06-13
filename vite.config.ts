9f8647dd9165c6c847b9900fefc76fa371ddbb5c
NISHIZAWA Shuntaro <me@s2n.tech>
2025-06-13 17:10:04 +0900

    chore(deps): vite-plugin-htmlを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		createHtmlPlugin({
-			minify: true,
-		}),
-		tsconfigPaths(),
-	],
+	plugins: [react(), tsconfigPaths()],
 });
