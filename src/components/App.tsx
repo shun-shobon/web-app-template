e80e48cd39336be96d65f023ce32e96ae3719445
NISHIZAWA Shuntaro <me@s2n.tech>
2025-12-22 01:53:55 +0900

    feat: Base UIを追加

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
+import { Button } from "@base-ui/react";
 import type { FC } from "react";
 
 const App: FC = () => {
 	return (
 		<div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
 			<h1 className="text-2xl font-bold">Hello, world!</h1>
-			<button className="rounded-md bg-blue-500 px-4 py-2 text-white">
+			<Button className="rounded-md bg-blue-500 px-4 py-2 text-white">
 				Click me
-			</button>
+			</Button>
 		</div>
 	);
 };
